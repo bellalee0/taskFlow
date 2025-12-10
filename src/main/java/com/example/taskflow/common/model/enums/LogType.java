ac23a1c64d4861ce158f70305f1b5a92467827df
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-10 17:31:29 +0900

    feat: LogType 이넘 설명 추가

--- src/main/java/com/example/taskflow/common/model/enums/LogType.java
+++ src/main/java/com/example/taskflow/common/model/enums/LogType.java
@@ -1,6 +1,19 @@
 package com.example.taskflow.common.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum LogType {
 
-    TASK_CREATED, TASK_UPDATED, TASK_DELETED, TASK_STATUS_CHANGED, COMMENT_CREATED, COMMENT_UPDATED, COMMENT_DELETED;
+    TASK_CREATED("작업 생성"),
+    TASK_UPDATED("작업 수정"),
+    TASK_DELETED("작업 삭제"),
+    TASK_STATUS_CHANGED("작업 상태 변경"),
+    COMMENT_CREATED("작업 작성"),
+    COMMENT_UPDATED("작업 수정"),
+    COMMENT_DELETED("작업 삭제");
+
+    private String action;
 }
