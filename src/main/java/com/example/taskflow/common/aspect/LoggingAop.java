6823cad946720a424cb1d82ea2652493e1103c5d
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-12 10:51:10 +0900

    feat: Task/Comment 작업 시, 활동 로그 기록
    - 기록 내용 : 실행 시간, 호출자, 메서드명, 입력 파라미터, 실행 결과, 예외 발생 여부

--- src/main/java/com/example/taskflow/common/aspect/LoggingAop.java
+++ src/main/java/com/example/taskflow/common/aspect/LoggingAop.java
@@ -5,14 +5,24 @@ import com.example.taskflow.common.entity.Log;
 import com.example.taskflow.common.entity.Task;
 import com.example.taskflow.common.entity.User;
 import com.example.taskflow.common.model.enums.LogType;
+import com.example.taskflow.common.model.response.GlobalResponse;
 import com.example.taskflow.domain.activities.repository.LogRepository;
+import com.example.taskflow.domain.comment.model.response.CommentCreateResponse;
+import com.example.taskflow.domain.comment.model.response.CommentUpdateResponse;
+import com.example.taskflow.domain.task.model.response.TaskCreateResponse;
+import com.example.taskflow.domain.task.model.response.TaskUpdateResponse;
+import com.example.taskflow.domain.task.model.response.TaskUpdateStatusResponse;
 import com.example.taskflow.domain.task.repository.TaskRepository;
 import com.example.taskflow.domain.user.repository.UserRepository;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -25,41 +35,40 @@ public class LoggingAop {
     private final UserRepository userRepository;
     private final TaskRepository taskRepository;
 
+    // Task, Comment 관련 작업 로깅
     @Around("@annotation(loggable)")
     public Object logExecution(ProceedingJoinPoint joinPoint, Loggable loggable) throws Throwable {
 
-        boolean exception = false;
         long start = System.currentTimeMillis();
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
         Object result = null;
 
         try {
             result = joinPoint.proceed();
         } catch (Throwable throwable) {
-            exception = true;
+            logInfo(joinPoint, start, username, result, true);
+            throw throwable;
         }
+
+        logInfo(joinPoint, start, username, result, false);
+
+        return result;
+    }
+
+    // 콘솔에 활동 로그 남기기
+    private void logInfo(ProceedingJoinPoint joinPoint, long start, String username, Object result, boolean exception) {
 
         long end = System.currentTimeMillis();
 
-        log.info(" 활동 로그 | 실행 시간={}ms | 호출자= | 메서드명 = . | 입력 파라미터={} | 실행 결과={} | 예외 발생={}",
+        log.info(" 활동 로그 : 실행 시간={}ms | 호출자={} | 메서드명={}.{} | 입력 파라미터={} | 실행 결과={} | 예외 발생 여부={}",
             (end - start),
+            username,
+            joinPoint.getSignature().getDeclaringType().getSimpleName(),
+            joinPoint.getSignature().getName(),
             joinPoint.getArgs(),
             result,
             exception
         );
-
-        LogType type = loggable.logType();
-
-        long userId = 1L;
-        User user = userRepository.findUserById(userId);
-
-        long taskId = 1L;
-        Task task = taskRepository.findTaskById(taskId);
-
-        String description = "설명";
-
-        Log log = new Log(type, user, task, description);
-        logRepository.save(log);
-
-        return result;
     }
 }
