2fd0b1da38712f0405d1e6261f795fb9dfb34149
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-12 11:33:55 +0900

    feat: description task 제목에 따옴표 추가

--- src/main/java/com/example/taskflow/common/model/enums/LogType.java
+++ src/main/java/com/example/taskflow/common/model/enums/LogType.java
@@ -7,11 +7,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum LogType {
 
-    TASK_CREATED("작업 생성", "새로운 작업 {title}을/를 생성했습니다."),
-    TASK_UPDATED("작업 수정", "작업 {title} 정보를 수정했습니다."),
-    TASK_DELETED("작업 삭제", "작업 {title}을 삭제했습니다."),
+    TASK_CREATED("작업 생성", "새로운 작업 \"{title}\"을/를 생성했습니다."),
+    TASK_UPDATED("작업 수정", "작업 \"{title}\" 정보를 수정했습니다."),
+    TASK_DELETED("작업 삭제", "작업 \"{title}\"을 삭제했습니다."),
     TASK_STATUS_CHANGED("작업 상태 변경", "작업 상태를 {oldStatus}에서 {newStatus}(으)로 변경했습니다."),
-    COMMENT_CREATED("댓글 작성", "작업 {title}에 댓글을 작성했습니다."),
+    COMMENT_CREATED("댓글 작성", "작업 \"{title}\"에 댓글을 작성했습니다."),
     COMMENT_UPDATED("댓글 수정", "댓글을 수정했습니다."),
     COMMENT_DELETED("댓글 삭제", "댓글을 삭제했습니다.");
 
