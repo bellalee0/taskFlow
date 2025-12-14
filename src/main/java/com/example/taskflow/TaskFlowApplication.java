d60e8e8b7f95a46a4c4cdecb0582a9ebeda458c0
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-12-14 15:25:51 +0900

    refactor: 사용하지 않는 import문 삭제

--- src/main/java/com/example/taskflow/TaskFlowApplication.java
+++ src/main/java/com/example/taskflow/TaskFlowApplication.java
@@ -2,7 +2,6 @@ package com.example.taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class TaskFlowApplication {
@@ -10,5 +9,4 @@ public class TaskFlowApplication {
     public static void main(String[] args) {
         SpringApplication.run(TaskFlowApplication.class, args);
     }
-
 }
